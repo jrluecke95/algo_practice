ba4434ea0dbf3b328517e05a49023e9efe2c1481
Jake Luecke <jrluecke95@gmail.com>
2021-03-18 17:26:24 -0400

    added coments

--- cycleSort.js
+++ cycleSort.js
@@ -1,22 +1,29 @@
 function cycleSort(array) {
+  // going over array until 3rd to last index
   for (let currIndex = 0; currIndex < array.length - 1; currIndex++) {
+    //setting item up to current index and making copy of index to keep track of where item will go
     let item = array[currIndex];
     let copyCurrIndex = currIndex;
+    //looping through items in array other than the current one we are using as item value
     for (let i = currIndex + 1; i < array.length; i++) {
+      //checking to see if item is greater than inner loop array value and adding one to its position if so (in order to determine final position in array)
       if (array[i] < item) {
         copyCurrIndex++;
       }
     }
+    // if it's position is correct - continue
     if (copyCurrIndex === currIndex) {
       continue
     }
+    // while the item is equal to duplicate values - move its  position - skipping duplicates
     while (item === array[copyCurrIndex]) {
       copyCurrIndex++;
     }
+    // swapping values
     let temp = array[copyCurrIndex];
     array[copyCurrIndex] = item;
     item = temp;
-
+    //looping over same code as above until whole array is sorted (which will have the copy and curr index equal each otehr since there is no array[i] < item check passing na diwll just skip through all conditionals)
     while (copyCurrIndex !== currIndex) {
       copyCurrIndex = currIndex;
       for (let i = currIndex + 1; i < array.length; i++) {
